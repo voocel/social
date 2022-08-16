b8900bde3a69ce127b7eda6e08cbaa87f417cc8c
voocel <voocel@gmail.com>
2022-08-16 23:55:02 +0800

    init

--- internal/entity/message.go
+++ internal/entity/message.go
@@ -7,3 +7,9 @@ type Message struct {
 	ContentType int16  `json:"content_type"` // 1.文字 2.普通文件 3.图片 4.音频 5.视频 6.语音聊天 7.视频聊天
 	Pic         string `json:"pic"`
 }
+
+type Response struct {
+	Code    uint32      `json:"code"`
+	Message string      `json:"message"`
+	Data    interface{} `json:"result"`
+}
