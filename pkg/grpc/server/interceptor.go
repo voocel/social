87808d2284959a6add23d3c756eeab988839ca55
voocel <voocel@gmail.com>
2022-10-12 23:40:49 +0800

    fix log

--- pkg/grpc/server/interceptor.go
+++ pkg/grpc/server/interceptor.go
@@ -3,6 +3,7 @@ package server
 import (
 	"context"
 	"log"
+	"runtime/debug"
 	"time"
 
 	"github.com/grpc-ecosystem/go-grpc-prometheus"
@@ -14,16 +15,18 @@ func UnaryServerPrometheus() grpc.UnaryServerInterceptor {
 	return grpc_prometheus.UnaryServerInterceptor
 }
 
-func UnaryServerInterceptor(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
+func UnaryServerInterceptor(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp interface{}, err error) {
 	head := metadata.Pairs("action", "123")
 	_ = grpc.SetHeader(ctx, head)
 
 	start := time.Now()
 	md, _ := metadata.FromIncomingContext(ctx)
-
-	log.Printf("before invoker. method: %+v, request:%+v, md: %v", info.FullMethod, req, md)
-	resp, err := handler(ctx, req)
-	log.Printf("before invoker. method: %+v, response:%+v, duration: %v", info.FullMethod, resp, time.Since(start))
-
-	return resp, err
+	defer func() {
+		if err := recover(); err != nil {
+			log.Printf("panic err: %v; stack: %v", err, string(debug.Stack()))
+		} else {
+			log.Printf("grpc invoker method: %+v, request:%+v, response:%+v, md: %v, duration: %v", info.FullMethod, req, resp, md, time.Since(start))
+		}
+	}()
+	return handler(ctx, req)
 }
