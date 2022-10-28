fccf7d8b49258611712277d7bf7aa83096fed87a
voocel <voocel@gmail.com>
2022-10-28 22:37:18 +0800

    add StatsHandler

--- pkg/grpc/server/builder.go
+++ pkg/grpc/server/builder.go
@@ -8,6 +8,7 @@ import (
 	"google.golang.org/grpc/health/grpc_health_v1"
 	"google.golang.org/grpc/keepalive"
 	"google.golang.org/grpc/reflection"
+	"google.golang.org/grpc/stats"
 )
 
 type GrpcServerBuilder struct {
@@ -25,7 +26,7 @@ func (b *GrpcServerBuilder) EnablePrometheus() {
 	b.enabledPrometheus = true
 }
 
-func (b *GrpcServerBuilder) EnabledHealth()  {
+func (b *GrpcServerBuilder) EnabledHealth() {
 	b.enabledHealth = true
 }
 
@@ -53,6 +54,18 @@ func (b *GrpcServerBuilder) SetTLSCert(serverKeyPath, serverPemPath, caPemPath s
 	b.AddOption(cred)
 }
 
+func (b *GrpcServerBuilder) SetUnknownServiceHandler(streamHandler grpc.StreamHandler) {
+	b.AddOption(grpc.UnknownServiceHandler(streamHandler))
+}
+
+func (b *GrpcServerBuilder) SetStatsHandler(handler ...stats.Handler) {
+	var h stats.Handler = &DefaultStatsHandler{}
+	if len(handler) > 0 {
+		h = handler[0]
+	}
+	b.AddOption(grpc.StatsHandler(h))
+}
+
 func (b *GrpcServerBuilder) Build() GrpcServer {
 	srv := grpc.NewServer(b.options...)
 	if b.enabledReflection {
@@ -74,6 +87,7 @@ func (b *GrpcServerBuilder) Build() GrpcServer {
 }
 
 type HealthImpl struct{}
+
 func (h *HealthImpl) Check(ctx context.Context, in *grpc_health_v1.HealthCheckRequest) (*grpc_health_v1.HealthCheckResponse, error) {
 	return &grpc_health_v1.HealthCheckResponse{
 		Status: grpc_health_v1.HealthCheckResponse_SERVING,
@@ -81,4 +95,32 @@ func (h *HealthImpl) Check(ctx context.Context, in *grpc_health_v1.HealthCheckRe
 }
 func (h *HealthImpl) Watch(in *grpc_health_v1.HealthCheckRequest, stream grpc_health_v1.Health_WatchServer) error {
 	return nil
+}
+
+type DefaultStatsHandler struct{}
+
+func (h *DefaultStatsHandler) TagConn(ctx context.Context, info *stats.ConnTagInfo) context.Context {
+	return ctx
+}
+func (h *DefaultStatsHandler) HandleConn(ctx context.Context, s stats.ConnStats) {
+	switch s.(type) {
+	case *stats.ConnBegin:
+	case *stats.ConnEnd:
+	default:
+	}
+}
+func (h *DefaultStatsHandler) TagRPC(ctx context.Context, info *stats.RPCTagInfo) context.Context {
+	return ctx
+}
+func (h *DefaultStatsHandler) HandleRPC(ctx context.Context, s stats.RPCStats) {
+	switch s.(type) {
+	case *stats.Begin:
+	case *stats.End:
+	case *stats.InHeader:
+	case *stats.InPayload:
+	case *stats.InTrailer:
+	case *stats.OutHeader:
+	case *stats.OutPayload:
+	default:
+	}
 }
