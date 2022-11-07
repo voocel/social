e194ea8d22d2795c33eef03aac597407046bb93a
voocel <voocel@gmail.com>
2022-11-07 23:51:03 +0800

    wip gateway option

---
+++ internal/app/gateway/options.go
@@ -0,0 +1,25 @@
+package gateway
+
+import (
+	"social/pkg/discovery"
+	"social/pkg/network"
+)
+
+type options struct {
+	id        string
+	name      string
+	server    network.Server
+	discovery discovery.Discovery
+}
+
+type OptionFunc func(o *options)
+
+func defaultOptions() *options {
+	return &options{}
+}
+
+func WithID(id string) OptionFunc { return func(o *options) { o.id = id } }
+
+func WithName(name string) OptionFunc { return func(o *options) { o.name = name } }
+
+func WithServer(s network.Server) OptionFunc { return func(o *options) { o.server = s } }
