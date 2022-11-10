d4ba5c8e8f34ec47facfccaa80866faa202ce01a
voocel <voocel@gmail.com>
2022-11-10 23:37:30 +0800

    wip

--- internal/app/im/im.go
+++ internal/app/im/im.go
@@ -3,21 +3,24 @@ package im
 import (
 	"os"
 	"os/signal"
+	"social/internal/node"
 	"social/pkg/log"
-	"social/pkg/websocket"
 	"syscall"
 )
 
-func Run()  {
-	ws := websocket.NewWsServer()
-	go ws.Run()
+func Run() {
+	n := node.NewNode()
+	core := NewCore(n.GetProxy())
+	core.Init()
+	n.Start()
 
 	ch := make(chan os.Signal, 1)
 	signal.Notify(ch, syscall.SIGQUIT, syscall.SIGTERM, syscall.SIGINT, syscall.SIGHUP)
 	for {
-		s := <-ch
-		switch s {
+		sig := <-ch
+		switch sig {
 		case syscall.SIGQUIT, syscall.SIGTERM, syscall.SIGINT:
+			n.Stop()
 			log.Sync()
 			return
 		case syscall.SIGHUP:
