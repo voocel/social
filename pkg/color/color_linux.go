ff591ca8dab857479420703f57b045d910521c40
voocel <voocel@gmail.com>
2022-11-16 23:09:38 +0800

    feat: add color

---
+++ pkg/color/color_linux.go
@@ -0,0 +1,37 @@
+package color
+
+import (
+	"fmt"
+	"math/rand"
+	"strconv"
+)
+
+var _ = RandomColor()
+
+func RandomColor() string {
+	return fmt.Sprintf("#%s", strconv.FormatInt(int64(rand.Intn(16777216)), 16))
+}
+
+func Yellow(msg string) string {
+	return fmt.Sprintf("\x1b[33m%s\x1b[0m", msg)
+}
+
+func Red(msg string) string {
+	return fmt.Sprintf("\x1b[31m%s\x1b[0m", msg)
+}
+
+func Redf(msg string, arg interface{}) string {
+	return fmt.Sprintf("\x1b[31m%s\x1b[0m %+v\n", msg, arg)
+}
+
+func Blue(msg string) string {
+	return fmt.Sprintf("\x1b[34m%s\x1b[0m", msg)
+}
+
+func Green(msg string) string {
+	return fmt.Sprintf("\x1b[32m%s\x1b[0m", msg)
+}
+
+func Greenf(msg string, arg interface{}) string {
+	return fmt.Sprintf("\x1b[32m%s\x1b[0m %+v\n", msg, arg)
+}
