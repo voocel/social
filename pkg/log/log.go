420100b197f833d5a9ad3406868f3597e0127c76
voocel <voocel@gmail.com>
2022-12-13 22:17:33 +0800

    fix log

--- pkg/log/log.go
+++ pkg/log/log.go
@@ -1,11 +1,11 @@
 package log
 
 import (
+	"context"
 	"net/http"
 	"os"
 	"path/filepath"
 	"runtime"
-	"strings"
 	"time"
 
 	"go.uber.org/zap"
@@ -15,20 +15,22 @@ import (
 
 const (
 	pattern  = "/log/level"
-	endpoint = ":4246"
+	endpoint = ":4247"
 )
-
-var logger *zap.SugaredLogger
-var atomicLevel = zap.NewAtomicLevel()
 
 var levelMap = map[string]zapcore.Level{
 	"debug": zapcore.DebugLevel,
 	"info":  zapcore.InfoLevel,
 	"warn":  zapcore.WarnLevel,
 	"error": zapcore.ErrorLevel,
-	"panic": zapcore.PanicLevel,
 	"fatal": zapcore.FatalLevel,
+	"panic": zapcore.PanicLevel,
 }
+
+var (
+	srv    *http.Server
+	logger *zap.SugaredLogger
+)
 
 func toZapLevel(l string) zapcore.Level {
 	if level, ok := levelMap[l]; ok {
@@ -38,26 +40,36 @@ func toZapLevel(l string) zapcore.Level {
 }
 
 func Init(serviceName, filePath, level string) {
-	http.HandleFunc(pattern, atomicLevel.ServeHTTP)
+	var atomicLevel = zap.NewAtomicLevel()
+	atomicLevel.SetLevel(toZapLevel(level))
+	mux := http.NewServeMux()
+	mux.HandleFunc(pattern, atomicLevel.ServeHTTP)
+	srv = &http.Server{
+		Addr:    endpoint,
+		Handler: mux,
+	}
+
 	go func() {
-		if err := http.ListenAndServe(endpoint, nil); err != nil {
+		if err := srv.ListenAndServe(); err != nil {
 			panic(err)
 		}
 	}()
 
-	// error, fatal, panic
-	highLevel := zap.LevelEnablerFunc(func(l zapcore.Level) bool {
-		return l >= zap.ErrorLevel
-	})
-	// info, debug
-	lowLever := zap.LevelEnablerFunc(func(l zapcore.Level) bool {
-		return l < zap.ErrorLevel && l >= toZapLevel(level)
-	})
+	//// error, fatal, panic
+	//highLevel := zap.LevelEnablerFunc(func(l zapcore.Level) bool {
+	//	return l >= zap.ErrorLevel
+	//})
+	//// info, debug
+	//lowLever := zap.LevelEnablerFunc(func(l zapcore.Level) bool {
+	//	return l < zap.ErrorLevel && l >= toZapLevel(level)
+	//})
+	//
+	//highCore := newCore(filePath, highLevel, "error.log")
+	//lowCore := newCore(filePath, lowLever, "info.log")
 
-	highCore := newCore(filePath, highLevel, "error.log")
-	lowCore := newCore(filePath, lowLever, "info.log")
+	core := newCore(filePath, atomicLevel)
 	log := zap.New(
-		zapcore.NewTee(highCore, lowCore),
+		zapcore.NewTee(core),
 		zap.AddCaller(),
 		zap.AddCallerSkip(1),
 		zap.Development(),
@@ -75,9 +87,8 @@ func funcName() string {
 	return filepath.Base(runtime.FuncForPC(pc).Name())
 }
 
-func newCore(filePath string, priority zap.LevelEnablerFunc, filename string) zapcore.Core {
-	filename = strings.Replace(filename, ".log", "", -1) +
-		"-" + time.Now().Format("2006-01-02") + ".log"
+func newCore(filePath string, atomicLevel zap.AtomicLevel) zapcore.Core {
+	filename := time.Now().Format("2006-01-02") + ".log"
 	logPath := filepath.Join(filepath.Dir(filePath), filename)
 	fileWriteSyncer := &lumberjack.Logger{
 		Filename:   logPath, // 日志文件存放目录
@@ -107,79 +118,78 @@ func newCore(filePath string, priority zap.LevelEnablerFunc, filename string) za
 			zapcore.AddSync(os.Stdout),
 			zapcore.AddSync(fileWriteSyncer),
 		),
-		priority,
+		atomicLevel,
 	)
 }
 
+// Debug .
 func Debug(args ...interface{}) {
 	logger.Debug(args...)
 }
-
 func Debugf(format string, args ...interface{}) {
 	logger.Debugf(format, args...)
 }
-
 func Debugw(msg string, keysAndValues ...interface{}) {
 	logger.Debugw(msg, keysAndValues...)
 }
 
+// Info .
 func Info(args ...interface{}) {
 	logger.Info(args...)
 }
-
 func Infof(format string, args ...interface{}) {
 	logger.Infof(format, args...)
 }
-
 func Infow(msg string, keysAndValues ...interface{}) {
 	logger.Infow(msg, keysAndValues...)
 }
 
+// Warn .
 func Warn(args ...interface{}) {
 	logger.Warn(args...)
 }
-
 func Warnf(format string, args ...interface{}) {
 	logger.Warnf(format, args...)
 }
-
 func Warnw(msg string, keysAndValues ...interface{}) {
 	logger.Warnw(msg, keysAndValues...)
 }
 
+// Error .
 func Error(args ...interface{}) {
 	logger.Error(args...)
 }
-
 func Errorf(format string, args ...interface{}) {
 	logger.Errorf(format, args...)
 }
-
 func Errorw(msg string, keysAndValues ...interface{}) {
 	logger.Errorw(msg, keysAndValues...)
 }
 
+// Fatal .
 func Fatal(args ...interface{}) {
 	logger.Fatal(args...)
 }
-
 func Fatalf(format string, args ...interface{}) {
 	logger.Fatalf(format, args...)
 }
+func Fatalw(msg string, keysAndValues ...interface{}) {
+	logger.Fatalw(msg, keysAndValues...)
+}
 
+// Panic .
 func Panic(args ...interface{}) {
 	logger.Panic(args...)
 }
-
 func Panicf(format string, args ...interface{}) {
 	logger.Panicf(format, args...)
 }
-
 func Panicw(msg string, keysAndValues ...interface{}) {
 	logger.Panicw(msg, keysAndValues...)
 }
 
 func Sync() error {
+	srv.Shutdown(context.Background())
 	if logger != nil {
 		return logger.Sync()
 	}
