d4d8cac271cc00527cf9ab61a1fe4c043426b147
voocel <voocel@gmail.com>
2023-01-11 01:15:07 +0800

    wip: postgres user interface

--- internal/usecase/interfaces.go
+++ internal/usecase/interfaces.go
@@ -2,19 +2,20 @@ package usecase
 
 import (
 	"context"
+	"social/ent"
 	"social/internal/entity"
 )
 
 type (
 	User interface {
-		GetUserById(ctx context.Context, uid int)(*entity.User, error)
+		GetUserById(ctx context.Context, uid int64) (*entity.User, error)
 	}
 
 	UserRepo interface {
-		GetUserByIdRepo(ctx context.Context, uid int)(*entity.User, error)
+		GetUserByIdRepo(ctx context.Context, uid int64) (*ent.User, error)
+		GetUserByNameRepo(ctx context.Context, name string) (*ent.User, error)
+		GetUsersRepo(ctx context.Context) ([]*ent.User, int, error)
 	}
 
-	UserWebAPI interface {
-
-	}
+	UserWebAPI interface{}
 )
