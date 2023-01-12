28ff0d2844ed7eae7e4f27e8b58cefe6be42e37f
voocel <voocel@gmail.com>
2023-01-12 21:54:10 +0800

    wip: user handle

--- internal/delivery/http/v1/user.go
+++ internal/delivery/http/v1/user.go
@@ -1,7 +1,115 @@
 package v1
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/copier"
+	"net/http"
+	"social/internal/app/http/middleware"
+	"social/internal/entity"
+	"social/internal/entity/filter"
+	"social/internal/usecase"
+	"time"
+)
 
-func Login(ctx *gin.Context) {
+type UserHandler struct {
+	userUsecase *usecase.UserUseCase
+}
+
+func NewUserHandler(userUsecase *usecase.UserUseCase) *UserHandler {
+	return &UserHandler{
+		userUsecase: userUsecase,
+	}
+}
+
+type ApiResponse struct {
+	Code    int         `json:"code"`
+	Message string      `json:"message"`
+	Data    interface{} `json:"data,omitempty"`
+}
+
+func (h *UserHandler) Login(c *gin.Context) {
+	req := entity.UserLoginReq{}
+	resp := new(ApiResponse)
+	if err := c.ShouldBind(&req); err != nil {
+		resp.Code = 1
+		resp.Message = "params invalid"
+		c.JSON(http.StatusOK, resp)
+		return
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+	user, err := h.userUsecase.UserLogin(ctx, req)
+	if err != nil {
+		resp.Code = 1
+		resp.Message = err.Error()
+		c.JSON(http.StatusOK, resp)
+		return
+	}
+
+	at, rt, err := middleware.GenerateToken(user)
+	if err != nil {
+		resp.Code = 1
+		resp.Message = "generate token fail"
+		c.JSON(http.StatusOK, resp)
+		return
+	}
+	result := map[string]interface{}{
+		"id":            user.ID,
+		"username":      user.Username,
+		"nickname":      user.Nickname,
+		"sex":           user.Sex,
+		"avatar":        user.Avatar,
+		"access_token":  at,
+		"refresh_token": rt,
+	}
+	resp.Message = "login successfully"
+	resp.Data = result
+	c.JSON(http.StatusOK, resp)
+}
+
+func (h *UserHandler) Register(c *gin.Context) {
+	req := entity.UserRegisterReq{}
+	resp := new(ApiResponse)
+	if err := c.ShouldBind(&req); err != nil {
+		resp.Code = 1
+		resp.Message = "params invalid"
+		c.JSON(http.StatusOK, resp)
+		return
+	}
+	m := make(map[string]string)
+	if err := copier.Copy(m, req); err != nil {
+		resp.Code = 1
+		resp.Message = "params invalid."
+		c.JSON(http.StatusOK, resp)
+		return
+	}
+	filter.Filters(m)
+	if err := h.userUsecase.UserRegister(c, req); err != nil {
+		resp.Code = 1
+		resp.Message = err.Error()
+		c.JSON(http.StatusOK, resp)
+		return
+	}
+	resp.Message = "register successfully"
+	c.JSON(http.StatusOK, resp)
+}
+
+func (h *UserHandler) Info(c *gin.Context) {
+	resp := new(ApiResponse)
+	user, exists := c.Get("user")
+	if !exists {
+		resp.Code = 1
+		resp.Message = "user not exists"
+		c.JSON(http.StatusOK, resp)
+		return
+	}
+	resp.Message = "ok"
+	resp.Data = user
+	c.JSON(http.StatusOK, resp)
+}
+
+func (h *UserHandler) RetrievePassword(c *gin.Context) {
 
 }
