4836e8c8dab63b5655c4ba8a4d831c02918e3c64
voocel <voocel@gmail.com>
2023-02-01 00:57:25 +0800

    fix node proxy

--- internal/node/proxy.go
+++ internal/node/proxy.go
@@ -1,6 +1,19 @@
 package node
 
-import "sync"
+import (
+	"context"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"log"
+	"social/protos/gate"
+	"sync"
+)
+
+var gateClients sync.Map
+
+type gateClient struct {
+	client gate.GateClient
+}
 
 type Proxy struct {
 	node       *Node
@@ -21,6 +34,37 @@ func (p *Proxy) SetDefaultRouteHandler(handler RouteHandler) {
 	p.node.defaultRouteHandler = handler
 }
 
-func (p *Proxy) newGateClient() {
+func (p *Proxy) BindGate(uid int64) {
+	c := p.getGateClient("")
+	c.client.Bind(context.Background(), &gate.BindRequest{
+		CID: 0,
+		UID: uid,
+	})
+}
+
+func (p *Proxy) getGateClient(addr string) *gateClient {
+	c, ok := gateClients.Load(addr)
+	if ok {
+		return c.(*gateClient)
+	}
+	conn, err := grpc.Dial("127.0.0.1:7400", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		panic(err)
+	}
+	cc := &gateClient{client: gate.NewGateClient(conn)}
+	gateClients.Store(addr, cc)
+	return cc
+}
+
+func (p *Proxy) Response(ctx context.Context, msg []byte) {
+	c := p.getGateClient("")
+	r, err := c.client.Push(ctx, &gate.PushRequest{Buffer: msg})
+	if err != nil {
+		log.Fatalf("could not gate: %v", err)
+	}
+	log.Printf("push: %s", r.String())
+}
+
+func (p *Proxy) directDeliver(ctx context.Context, args *DeliverArgs) {
 
 }
