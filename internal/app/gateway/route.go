8461d3a8f9f6cb4f0bda95a7635195b88af5f85d
voocel <voocel@gmail.com>
2023-02-18 23:46:55 +0800

    route

---
+++ internal/app/gateway/route.go
@@ -0,0 +1,31 @@
+package gateway
+
+import "sync"
+
+type Route struct {
+	id        int32    // 路由ID
+	stateful  bool     // 是否有状态
+	endpoints sync.Map // 服务端口
+	strategy  string   // 负载均衡策略
+}
+
+type Router struct {
+	rw            sync.RWMutex
+	routes        map[int32]*Route  // 节点路由表
+	gateEndpoints map[string]string // 网关服务端口
+	nodeEndpoints map[string]string // 节点服务端口
+}
+
+func NewRouter() *Router {
+	return &Router{
+		routes:        make(map[int32]*Route),
+		gateEndpoints: make(map[string]string),
+		nodeEndpoints: make(map[string]string),
+	}
+}
+
+func (r *Router) addService(id, addr string) {
+	r.rw.Lock()
+	defer r.rw.Unlock()
+	r.gateEndpoints[id] = addr
+}
