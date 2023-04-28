302ec81635913b2d621f88041a08cfb8178251bf
voocel <voocel@gmail.com>
2023-04-28 23:27:01 +0800

    fix group start

--- cmd/group/main.go
+++ cmd/group/main.go
@@ -1,13 +1,31 @@
 package main
 
 import (
+	"os"
+	"os/signal"
 	"social/config"
 	"social/internal/app/group"
 	"social/pkg/log"
+	"syscall"
 )
 
 func main() {
 	config.LoadConfig()
 	log.Init("group", "debug")
-	group.Run()
+	n := group.Run()
+
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, syscall.SIGQUIT, syscall.SIGTERM, syscall.SIGINT, syscall.SIGHUP)
+	for {
+		sig := <-ch
+		switch sig {
+		case syscall.SIGQUIT, syscall.SIGTERM, syscall.SIGINT:
+			n.Stop()
+			log.Sync()
+		case syscall.SIGHUP:
+			config.LoadConfig()
+		default:
+			return
+		}
+	}
 }
--- internal/app/group/group.go
+++ internal/app/group/group.go
@@ -2,15 +2,11 @@ package group
 
 import (
 	"github.com/spf13/viper"
-	"os"
-	"os/signal"
 	"social/internal/node"
 	"social/pkg/discovery"
-	"social/pkg/log"
-	"syscall"
 )
 
-func Run() {
+func Run() *node.Node {
 	n := node.NewNode(&discovery.Node{
 		Name: "group",
 		Host: viper.GetString("group.host"),
@@ -19,18 +15,5 @@ func Run() {
 	core := newCore(n.GetProxy())
 	core.Init()
 	n.Start()
-
-	ch := make(chan os.Signal, 1)
-	signal.Notify(ch, syscall.SIGQUIT, syscall.SIGTERM, syscall.SIGINT, syscall.SIGHUP)
-	for {
-		sig := <-ch
-		switch sig {
-		case syscall.SIGQUIT, syscall.SIGTERM, syscall.SIGINT:
-			log.Sync()
-			return
-		case syscall.SIGHUP:
-		default:
-			return
-		}
-	}
+	return n
 }
