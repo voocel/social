f580d541da71018c7e7b05f17fc8a005f0c0acd8
voocel <voocel@gmail.com>
2023-05-08 23:20:58 +0800

    feat: friend use singleflight

--- internal/usecase/friend.go
+++ internal/usecase/friend.go
@@ -2,11 +2,13 @@ package usecase
 
 import (
 	"context"
+	"golang.org/x/sync/singleflight"
 	"social/ent"
 )
 
 type FriendUseCase struct {
 	repo FriendRepo
+	sf   singleflight.Group
 }
 
 func NewFriendUseCase(r FriendRepo) *FriendUseCase {
@@ -14,5 +16,8 @@ func NewFriendUseCase(r FriendRepo) *FriendUseCase {
 }
 
 func (f *FriendUseCase) GetFriendsRepo(ctx context.Context, uid int64) ([]*ent.Friend, error) {
-	return f.repo.GetFriendsRepo(ctx, uid)
+	v, err, _ := f.sf.Do("key", func() (interface{}, error) {
+		return f.repo.GetFriendsRepo(ctx, uid)
+	})
+	return v.([]*ent.Friend), err
 }
