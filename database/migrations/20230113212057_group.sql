8f5dd76cf54ef8f3dac024f036342d13e6b55dfd
voocel <voocel@gmail.com>
2023-07-11 23:29:32 +0800

    add group sql

--- database/migrations/20230113212057_group.sql
+++ database/migrations/20230113212057_group.sql
@@ -1,9 +1,17 @@
 -- +goose Up
 -- +goose StatementBegin
-SELECT 'up SQL query';
+CREATE TABLE IF NOT EXISTS group(
+    id         BIGSERIAL PRIMARY KEY NOT NULL,
+    name       varchar(32) NOT NULL DEFAULT '未命名',
+    owner      smallint NOT NULL DEFAULT 0,
+    notice     varchar(255) NOT NULL DEFAULT '',
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP(0),
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP(0),
+    deleted_at TIMESTAMP,
+)
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
-SELECT 'down SQL query';
+drop table group;
 -- +goose StatementEnd
