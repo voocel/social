b71c06417a982b59009b75383eeab3106bf6d4cc
voocel <voocel@gmail.com>
2023-08-19 20:35:55 +0800

    wip: support provider

---
+++ internal/app/gateway/provider.go
@@ -0,0 +1,40 @@
+package gateway
+
+import (
+	"context"
+	"social/internal/session"
+)
+
+type provider struct {
+	gate *Gateway
+}
+
+func (p provider) Session(target int64) (*session.Session, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (p provider) Bind(ctx context.Context, uid int64) error {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (p provider) Unbind(ctx context.Context, uid int64) error {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (p provider) Push(target int64, msg []byte, msgType ...int) error {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (p provider) Multicast(targets []int64, msg []byte, msgType ...int) (n int, err error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (p provider) Broadcast(msg []byte, msgType ...int) (n int, err error) {
+	//TODO implement me
+	panic("implement me")
+}
---
+++ internal/node/provider.go
@@ -0,0 +1,37 @@
+package node
+
+import (
+	"fmt"
+	"social/internal/transport"
+	"social/pkg/log"
+)
+
+type provider struct {
+	node *Node
+}
+
+func (p provider) Trigger(event Event, gid string, uid int64) {
+	p.node.triggerEvent(event, gid, uid)
+}
+
+func (p provider) Deliver(gid, nid string, cid, uid int64, message *transport.Message) {
+	route, ok := p.node.Routes[message.Route]
+	r := Request{
+		Gid:    gid,
+		Nid:    nid,
+		Cid:    cid,
+		Uid:    uid,
+		Route:  message.Route,
+		Buffer: message.Buffer,
+		Node:   p.node,
+	}
+	var err error
+	if ok {
+		err = route.Handler(r)
+	} else if p.node.DefaultRouteHandler != nil {
+		err = p.node.DefaultRouteHandler(r)
+	} else {
+		err = fmt.Errorf("the route does not match: %v", message.Route)
+	}
+	log.Debugf("[node] handle route err: %v", err)
+}
