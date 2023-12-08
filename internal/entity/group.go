466fcb53a6de24feebc58c88a4969ffc81da74a4
voocel <voocel@gmail.com>
2023-12-08 22:31:47 +0800

    feat: add group max members ent

--- internal/entity/group.go
+++ internal/entity/group.go
@@ -6,6 +6,7 @@ type Group struct {
 	Owner        int64  `json:"owner"`
 	Avatar       string `json:"avatar"`
 	Notice       string `json:"notice"`
+	MaxMembers   int    `json:"max_members"`
 	Introduction string `json:"introduction"`
 	CreatedUid   int64  `json:"created_uid"`
 }
